06d73d456cb05ba76ab5c4f84ba2f42699266360
BodNS <bondarenokn@gmail.com>
2021-04-18 12:47:27 +0300

    add create Task

---
+++ src/db/config/config.js
@@ -0,0 +1,14 @@
+require('dotenv/config');
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postgres',
+    database: process.env.DB_NAME || 'tododb20202',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: "postgres",
+    migrationStorage: 'json',
+    seederStorage: 'json'
+  },
+  test: {},
+  production: {},
+};
