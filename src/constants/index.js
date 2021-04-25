937490309d3e14ed4ad4dc0b544feca4abbabfb2
BodNS <bondarenokn@gmail.com>
2021-04-25 12:05:27 +0300

    validationMV was created

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+export const NAME_PATTER = /^[A-Z][a-z]{0,64}$/;
+export const LOGIN_PATTER = /^(?!.*?[\\/\[\]:;=,+*?<>]).{6,16}$/;
+export const PASSWORD_PATTER = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)[A-Za-z0-9_@#%!?^\-]{8,63}$/;
+
+export const ROLE = Object.freeze({
+    USER: 'USER',
+    ADMIN: 'ADMIN',
+    MODERATOR: 'MODERATOR',
+});
+
+export const ACTION = OBJECT.freeze({
+    CREATE: 'CREATE',
+    READ: 'READ',
+    UPDATE: 'UPDATE',
+    DELETE: 'DELETE',
+});
---
+++ src/utils/validation/task.js
@@ -0,0 +1,8 @@
+import Joi from '@hapi/joi';
+const valueSchema = Joi.string().min(1).max(255);
+const deadlineSchema = Joi.date();
+export default Joi.object({
+    isDone: Joi.boolean(). label('Is done').optional(),
+    value: valueSchema.optional().when('$isCreateMode', {then: valueSchema.required()}),
+    deadline: deadlineSchema.optional().when('$isCreateMode', {then: deadlineSchema.required()}),
+})
